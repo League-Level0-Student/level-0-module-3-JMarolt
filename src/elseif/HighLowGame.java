caf42687cd6dbc620914ffb1c5c7d8550170a20c
plooba <league@wts13.attlocal.net>
2017-11-18 16:00:39 -0800

    high low game and dizzyrobot

--- src/elseif/HighLowGame.java
+++ src/elseif/HighLowGame.java
@@ -5,20 +5,35 @@ package elseif;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class HighLowGame {
 
 	public static void main(String[] args) {
 		// 3. Change this line to give you a random number between 1 - 100. 
-		int random = new Random().nextInt(5);
+		int random = new Random().nextInt(1000000);
 		// 2. Print out the random variable above
 		
 		// 11. do the following 10 times
 		
 			// 1. ask the user for a guess using a pop-up window, and save their response 
-
+		for(int i = 0;i<10; i ++) {
+String number = JOptionPane.showInputDialog("Guess the number between 1 and 100");
+		
 			// 4. convert the users’ answer to an int (Integer.parseInt(string))
-			
+			int number2 = Integer.parseInt(number);
 			// 5. if the guess is correct
+			if(number2 < random) {
+			JOptionPane.showMessageDialog(null, "Too low");
+			
+			}
+			if(number2 > random) {
+				JOptionPane.showMessageDialog(null, "Too high");
+			}
+			
+			if(number2 == random) {
+				JOptionPane.showMessageDialog(null, "You win!");
+			}
 				// 6. win
 			// 7. if the guess is high
 				// 8. tell them it's too high
@@ -28,6 +43,7 @@ public class HighLowGame {
 		// 12. tell them they lose
 	}
 
+}
 }
 
 
--- src/extras/DizzyRobot.java
+++ src/extras/DizzyRobot.java
@@ -3,19 +3,29 @@
 
 package extras;
 
+import javax.swing.JOptionPane;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class DizzyRobot {
 	static Robot robot = new Robot();
 	public static void main(String[] args) {
-		
+		robot.setSpeed(10);
 		 // 2. Ask the user how dizzy you want the robot from 1-10, then spin that number of times.
+String question = JOptionPane.showInputDialog("How dizzy do you want your robot from a scale of 1 - 10?");
+ int numberOfSpins = Integer.parseInt(question);
 
  // 1. Use the dance method to make the robot spin.
+
 	
 
-	}
 
+dance(numberOfSpins);
+
+
+}
+	
+	
 	static void dance(int numberOfSpins) {
 		for (int i = 0; i < numberOfSpins; i++) {
 			robot.turn(360);
